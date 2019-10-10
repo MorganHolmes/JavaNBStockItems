0f5cb7d28c68bb6590c7f55fe9cb3774fff2bf05
Morgan Holmes <morgan.holmes@live.co.uk>
2019-10-10 18:03:50 +0100

    Task 4. Added user input.

---
+++ MWH_NetBeans_Stock-Items/src/Warehouse/Sales.java
@@ -0,0 +1,15 @@
+package Warehouse;
+
+import java.util.*;
+
+public class Sales {
+    
+    public static void main(String[] args) {
+       Scanner myObj = new Scanner(System.in);
+       System.out.println("Enter Product Name?");
+       String proName = myObj.nextLine();
+       StockItems product1 = new StockItems(proName, 65);
+       System.out.println(product1.getStockInfo());
+    }
+    
+}
--- MWH_NetBeans_Stock-Items/src/Warehouse/Tester.java
+++ MWH_NetBeans_Stock-Items/src/Warehouse/Tester.java
@@ -1,7 +1,7 @@
 package Warehouse;
 public class Tester
 {
-   public static void main(String[] args){
+   public static void main2(String[] args){
        StockItems test = new StockItems("Baked Beans 415g",65,10,10,2019);
        //System.out.println("Done");
        System.out.println(test.getStockInfo());
