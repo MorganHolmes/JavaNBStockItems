03e8facb6ce2fba7c61a42038726cf29204cfc79
Morgan Holmes <morgan.holmes@live.co.uk>
2019-10-17 17:26:33 +0100

    Task 4.6.

--- MWH_NetBeans_Stock-Items/src/Warehouse/StockList.java
+++ MWH_NetBeans_Stock-Items/src/Warehouse/StockList.java
@@ -15,7 +15,26 @@ public class StockList {
         shop.add(i);
     }
     
-    public int noOfStockItem(){
-        return 1;
+    public int noOfStockItems(){
+        //The size method gets the number of items in the Array List.
+        return shop.size();
+    }
+    
+    public void removeItem(int x){
+        if(x >= 0 && x < noOfStockItems()){
+            //Uses the remove method to remove the value sorted at index X
+            shop.remove(x);
+        }else{
+            System.out.println("Invalid Value Has Been Entered");
+        }
+    }
+    
+    public String getAllItems(){
+        String allItems = "";
+        for(int i = 0; i < noOfStockItems();i++){
+            StockItems temp = shop.get(i);
+            allItems += temp.getStockInfo();
+        }
+        return allItems;
     }
 }
